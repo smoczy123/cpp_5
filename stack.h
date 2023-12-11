6dcfe823122b8687bef207dd8eec19754fb4f1ed
smoczy123 <michal.hudobski@poczta.onet.pl>
2023-12-11 12:25:34 +0100

    first implementation (doesn't compile, circular dependency)

--- stack.h
+++ stack.h
@@ -1,35 +1,119 @@
 
 #include <utility>
+#include <stack>
+#include <map>
+#include <list>
 
 namespace cxx {
     template <typename K, typename V>
     class stack {
+    private:
+        using map_iter = typename std::map<K, std::stack<std::pair<list_iter, V>>>::iterator;
+        using cmap_iter = typename std::map<K, std::stack<std::pair<list_iter, V>>>::const_iterator;
+        using list_iter = typename std::list<map_iter>::iterator;
+        std::list<map_iter> actual_stack;
+        std::map<K, std::stack<std::pair<list_iter, V>>> access_map;
     public:
-        stack();
+        stack() noexcept {
+            actual_stack = std::list<map_iter>();
+            access_map = std::map<K, std::stack<std::pair<list_iter, V>>>();
+        }
 
-        stack(stack const &);
+        stack(stack const &) noexcept;
 
-        stack(stack &&);
+        stack(stack &&) noexcept;
 
         stack & operator=(stack);
 
-        void push(K const &, V const &);
+        void push(K const & key, V const & value) {
+            if (access_map.count(key) == 0) {
+                access_map.insert({key, std::stack<std::pair<list_iter, V>>()});
+            }
 
-        void pop();
+            actual_stack.push_front(access_map.find(key));
+            access_map.at(key).push({actual_stack.begin(), value});
+        }
 
-        void pop(K const &);
+        void pop() {
+            auto iter = actual_stack.front();
+            actual_stack.pop_front();
+            iter->second.pop();
+        }
 
-        std::pair<K const &, V &> front();
-        std::pair<K const &, V const &> front() const;
+        void pop(K const & key) {
+            std::pair<list_iter, V> pair = access_map.at(key).top();
+            actual_stack.erase(pair.first);
+            access_map.at(key).pop();
+        }
 
-        V & front(K const &);
-        V const & front(K const &) const;
+        std::pair<K const &, V &> front() {
+            auto iter = actual_stack.front();
+            return std::pair<K const &, V&>({iter.first, iter.second.top().second});
+        }
+        std::pair<K const &, V const &> front() const {
+            auto iter = actual_stack.front();
+            return std::pair<K const &, V const &>({iter.first, iter.second.top().second});
+        }
 
-        std::size_t size() const;
+        V & front(K const & key) {
+            return access_map.at(key).top();
+        }
+        V const & front(K const & key) const {
+            return access_map.at(key).top();
+        }
 
-        std::size_t count(K const &) const;
+        std::size_t size() const {
+            return actual_stack.size();
+        }
 
-        void clear();
+        std::size_t count(K const & key) const {
+            return access_map.at(key).size();
+        }
 
+        void clear() {
+            actual_stack.clear();
+            access_map.clear();
+        }
+
+
+
+        class const_iterator {
+            using iterator_category = std::forward_iterator_tag;
+            using value_type = K;
+            using pointer = K*;
+            using reference = K&;
+            cmap_iter iter;
+
+            const_iterator(cmap_iter i) : iter(i) {}
+
+        public:
+            const_iterator &operator++() noexcept {
+                iter++;
+                return *this;
+            }
+
+            const_iterator operator++(int) noexcept {
+                const_iterator tmp = *this;
+                ++(*this);
+                return tmp;
+            }
+
+            reference operator*() const {
+                return iter->first;
+            }
+
+            pointer operator->() const {
+                return &(iter->first);
+            }
+            friend bool operator== (const const_iterator& a, const const_iterator& b) { return a.iter == b.iter; };
+            friend bool operator!= (const const_iterator& a, const const_iterator& b) { return a.iter != b.iter; };
+        };
+
+        stack<K, V>::const_iterator cbegin() {
+            return const_iterator(access_map.cbegin());
+        }
+        stack<K, V>::const_iterator cend() {
+            return const_iterator(access_map.cend());
+        }
     };
 }
